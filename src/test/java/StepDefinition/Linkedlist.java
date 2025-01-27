c4c57c1ae8812d1bfd7706947ff1bc0d9f557e48
PremkumariGovindan <141989791+PremkumariGovindan@users.noreply.github.com>
2025-01-26 23:53:40 -0500

    Step definition file is created

--- src/test/java/StepDefinition/Linkedlist.java
+++ src/test/java/StepDefinition/Linkedlist.java
@@ -1,5 +1,243 @@
 package StepDefinition;
 
-public class Linkedlist {
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
+public class Linkedlist {
+	@Given("User is in Home screen after logging in")
+	public void User_is_in_Home_screen_after_logging_in() { 
+
+ 	}
+
+	@When("User clicks on Get Started button in Linked List panel")
+	public void User_clicks_on_Get_Started_button_in_Linked_List_panel() { 
+
+ 	}
+
+	@Then("User should be redirected to the LinkedList page")
+	public void User_should_be_redirected_to_the_LinkedList_page() { 
+
+ 	}
+
+	@Given("User is in LinkedList page")
+	public void User_is_in_LinkedList_page() { 
+
+ 	}
+
+	@When("User clicks on Introduction link")
+	public void User_clicks_on_Introduction_link() { 
+
+ 	}
+
+	@Then("User should be redirected to the Introduction page")
+	public void User_should_be_redirected_to_the_Introduction_page() { 
+
+ 	}
+
+	@Given("User is in Introduction page")
+	public void User_is_in_Introduction_page() { 
+
+ 	}
+
+	@When("User clicks on Try here>>> button")
+	public void User_clicks_on_Try_here_button() { 
+
+ 	}
+
+	@Then("User should be redirected to TryEditor page")
+	public void User_should_be_redirected_to_TryEditor_page() { 
+
+ 	}
+
+	@Given("User is in TryEditor page and provided invalid data")
+	public void User_is_in_TryEditor_page_and_provided_invalid_data() { 
+
+ 	}
+
+	@When("User clicks on Run button")
+	public void User_clicks_on_Run_button() { 
+
+ 	}
+
+	@Then("Alert message should be popped up")
+	public void Alert_message_should_be_popped_up() { 
+
+ 	}
+
+	@Given("User is in TryEditor page and provided valid python code")
+	public void User_is_in_TryEditor_page_and_provided_valid_python_code() { 
+
+ 	}
+
+	@Then("Python code should be successfully executed without any error")
+	public void Python_code_should_be_successfully_executed_without_any_error() { 
+
+ 	}
+
+	@Given("User in in TryEditor Page")
+	public void User_in_in_TryEditor_Page() { 
+
+ 	}
+
+	@When("User clicks on back button")
+	public void User_clicks_on_back_button() { 
+
+ 	}
+
+	@Then("User should be able to navigate to Introudction Page")
+	public void User_should_be_able_to_navigate_to_Introudction_Page() { 
+
+ 	}
+
+	@When("User clicks on Creating LinkedList link")
+	public void User_clicks_on_Creating_LinkedList_link() { 
+
+ 	}
+
+	@Then("User should be redirected to Creating LinkedList page")
+	public void User_should_be_redirected_to_Creating_LinkedList_page() { 
+
+ 	}
+
+	@Given("User is in Creating LinkedList page")
+	public void User_is_in_Creating_LinkedList_page() { 
+
+ 	}
+
+	@Then("User should be able to navigate to Creating Linked List Page")
+	public void User_should_be_able_to_navigate_to_Creating_Linked_List_Page() { 
+
+ 	}
+
+	@When("User clicks on Types of LinkedList link")
+	public void User_clicks_on_Types_of_LinkedList_link() { 
+
+ 	}
+
+	@Then("User should be redirected to Types of LinkedList page")
+	public void User_should_be_redirected_to_Types_of_LinkedList_page() { 
+
+ 	}
+
+	@Given("User is in Types of LinkedList page")
+	public void User_is_in_Types_of_LinkedList_page() { 
+
+ 	}
+
+	@Then("User should be able to navigate to Types of Linked List Page")
+	public void User_should_be_able_to_navigate_to_Types_of_Linked_List_Page() { 
+
+ 	}
+
+	@When("User clicks on Implement LinkedList in Python link")
+	public void User_clicks_on_Implement_LinkedList_in_Python_link() { 
+
+ 	}
+
+	@Then("User should be redirected to Implement LinkedList in Python page")
+	public void User_should_be_redirected_to_Implement_LinkedList_in_Python_page() { 
+
+ 	}
+
+	@Given("User is in Implement LinkedList in Python page")
+	public void User_is_in_Implement_LinkedList_in_Python_page() { 
+
+ 	}
+
+	@Then("User should be able to navigate to Implement LinkedList in Python Page")
+	public void User_should_be_able_to_navigate_to_Implement_LinkedList_in_Python_Page() { 
+
+ 	}
+
+	@When("User clicks on Traversal link")
+	public void User_clicks_on_Traversal_link() { 
+
+ 	}
+
+	@Then("User should be redirected to Traversal page")
+	public void User_should_be_redirected_to_Traversal_page() { 
+
+ 	}
+
+	@Given("User is in Traversal page")
+	public void User_is_in_Traversal_page() { 
+
+ 	}
+
+	@Then("User should be able to navigate to Traversal Page")
+	public void User_should_be_able_to_navigate_to_Traversal_Page() { 
+
+ 	}
+
+	@When("User clicks on Insertion link")
+	public void User_clicks_on_Insertion_link() { 
+
+ 	}
+
+	@Then("User should be redirected to Insertion page")
+	public void User_should_be_redirected_to_Insertion_page() { 
+
+ 	}
+
+	@Given("User is in Insertion page")
+	public void User_is_in_Insertion_page() { 
+
+ 	}
+
+	@Then("User should be able to navigate to Insertion Page")
+	public void User_should_be_able_to_navigate_to_Insertion_Page() { 
+
+ 	}
+
+	@When("User clicks on Deletion link")
+	public void User_clicks_on_Deletion_link() { 
+
+ 	}
+
+	@Then("User should be redirected to Deletion page")
+	public void User_should_be_redirected_to_Deletion_page() { 
+
+ 	}
+
+	@Given("User is in Deletion page")
+	public void User_is_in_Deletion_page() { 
+
+ 	}
+
+	@Then("User should be able to navigate to Deletion Page")
+	public void User_should_be_able_to_navigate_to_Deletion_Page() { 
+
+ 	}
+
+	@Given("User in in Introduction Page")
+	public void User_in_in_Introduction_Page() { 
+
+ 	}
+
+	@When("User selects Linked List option")
+	public void User_selects_Linked_List_option() { 
+
+ 	}
+
+	@Then("User should be able to see Linked List selected in the drop down")
+	public void User_should_be_able_to_see_Linked_List_selected_in_the_drop_down() { 
+
+ 	}
+
+	@Given("User in in Deletion Page")
+	public void User_in_in_Deletion_Page() { 
+
+ 	}
+
+	@When("User clicks Practice Questions link")
+	public void User_clicks_Practice_Questions_link() { 
+
+ 	}
+
+	@Then("User should be able to navigate to Practice Questions page and page content should be displayed")
+	public void User_should_be_able_to_navigate_to_Practice_Questions_page_and_page_content_should_be_displayed() { 
+
+ 	}
 }
+

0f6969422cc2eb19aff015942d55a880db8d7fc3
Reshmi <r.reshmi87@gmail.com>
2025-01-27 18:26:52 -0800

    Array and Datastructure files added

--- src/test/java/StepDefinition/Array.java
+++ src/test/java/StepDefinition/Array.java
@@ -1,5 +1,185 @@
 package StepDefinition;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Array {
+	@Given("The user is in the Home page after Sign in")
+	public void the_user_is_in_the_home_page_after_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the {string} button in Array panel")
+	public void the_user_clicks_the_button_in_array_panel(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should able to view {string} Page")
+	public void the_user_should_able_to_view_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user select Array item from the drop down menu")
+	public void the_user_select_array_item_from_the_drop_down_menu() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is in the {string} page after Sign in")
+	public void the_user_is_in_the_page_after_sign_in(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks {string} link")
+	public void the_user_clicks_link(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to {string} page")
+	public void the_user_should_be_redirected_to_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the {string} page")
+	public void the_user_is_on_the_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks {string} button in Arrays in Python page")
+	public void the_user_clicks_button_in_arrays_in_python_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to a page having an try Editor with a Run button to test")
+	public void the_user_should_be_redirected_to_a_page_having_an_try_editor_with_a_run_button_to_test() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is in the try Editor page")
+	public void the_user_is_in_the_try_editor_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the Run Button without entering the code in the Editor")
+	public void the_user_clicks_the_run_button_without_entering_the_code_in_the_editor() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should able to see an error message in alert window")
+	public void the_user_should_able_to_see_an_error_message_in_alert_window() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the invalid code in Editor and click the Run Button")
+	public void the_user_write_the_invalid_code_in_editor_and_click_the_run_button1() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the valid code in Editor and click the Run Button")
+	public void the_user_write_the_valid_code_in_editor_and_click_the_run_button1() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should able to see output in the console")
+	public void the_user_should_able_to_see_output_in_the_console() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is in the Array page after Sign in")
+	public void the_user_is_in_the_array_page_after_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks {string} button")
+	public void the_user_clicks_button(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks {string} button in Arrays Using List page")
+	public void the_user_clicks_button_in_arrays_using_list_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is in the Array page after Sign in	When The user clicks {string} button")
+	public void the_user_is_in_the_array_page_after_sign_in_when_the_user_clicks_button(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks {string} button in Basic Operations in Lists page")
+	public void the_user_clicks_button_in_basic_operations_in_lists_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks {string} button in Applications of Array")
+	public void the_user_clicks_button_in_applications_of_array(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the practice question editor")
+	public void the_user_is_on_the_practice_question_editor() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the invalid code in Editor and Click the Run Button")
+	public void the_user_write_the_invalid_code_in_editor_and_click_the_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the valid code in Editor and Click the Run Button")
+	public void the_user_write_the_valid_code_in_editor_and_click_the_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the invalid code in Editor and Click the Submit Button")
+	public void the_user_write_the_invalid_code_in_editor_and_click_the_submit_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user see an error message {string}")
+	public void the_user_see_an_error_message(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the valid code in Editor and Click the Submit Button")
+	public void the_user_write_the_valid_code_in_editor_and_click_the_submit_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user see success message {string}")
+	public void the_user_see_success_message(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
 
 }
